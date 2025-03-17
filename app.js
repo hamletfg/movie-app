2abd678952ea27120591d8fb0c93153ee10ac2d2
hamletfg <hamletfg@gmail.com>
2025-03-17 09:23:56 -0600

    Add test cards in html and refactor some code too

--- app.js
+++ app.js
@@ -26,24 +26,3 @@ async function fetchAndDisplayFirstMovie() {
 
 // Run on page load
 window.onload = fetchAndDisplayFirstMovie;
-
-// API Test: Fetch popular movies on page load
-// async function fetchPopularMovies() {
-//   try {
-//     // Make api call to TMDB's "popular" endpoint
-//     const response = await fetch(
-//       `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-//     );
-
-//     // Convert response to JSON
-//     const data = await response.json();
-
-//     // Log results to console to see if it works
-//     console.log("Popular Movies:", data.results);
-//   } catch (error) {
-//     console.error("Opps, API error:", error);
-//   }
-// }
-
-// // Call the function when the page loads
-// window.onload = fetchPopularMovies;
